2e4f30a688726017cebc87173536d2a57a901dc0
noodlescandy <conquerorix@gmail.com>
2023-03-29 16:06:09 -0400

    test point - parse inputs from post and print them

--- attendanceRptAction.php
+++ attendanceRptAction.php
@@ -1,11 +1,34 @@
 <?php
 require 'credentials.php';
-$event = $_POST['event'];
-if($event !== 'event_name'){
+function parseInput($in, $toCheckFor, $text, $shouldUnserialize){
+    if($in === $toCheckFor){
+        $in = [$toCheckFor, $text];
+    }
+    else{
+        if ($shouldUnserialize) {
+            $in = unserialize($in);
+        }
+        
+    }
+}
+
+/*$event = $_POST['event'];
+if($event === 'event_name'){
+    $event = ['event_name', 'All Events'];
+}
+else{
     $event = unserialize($event);
-}
-$student = $_POST['student'];
-if($student !== 'name'){
-    $student = unserialize($student);
-}
+}*/
+
+$event = parseInput($_POST['event'], 'event_name', 'All Events', true)
+$student = parseInput($_POST['student'], 'name', 'All Students', true);
+
+// these may cause problems
+$startDate = parseInput($_POST['startDate'] null, 'None', false);
+$endDate = parseInput($_POST['endDate'] null, 'None', false);
+
+echo "<p>$event</p><br>";
+echo "<p>$student</p><br>";
+echo "<p>Begin Date: $startDate End Date: $endDate</p><br>";
+
 ?>
