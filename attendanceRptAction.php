a2566753adebc9b402ec6b3be2e8f3629574661a
noodlescandy <conquerorix@gmail.com>
2023-03-29 16:19:28 -0400

    fixing errors

--- attendanceRptAction.php
+++ attendanceRptAction.php
@@ -1,15 +1,15 @@
 <?php
 require 'credentials.php';
 function parseInput($in, $toCheckFor, $text, $shouldUnserialize){
-    if($in === $toCheckFor){
+    if($in == $toCheckFor){
         $in = [$toCheckFor, $text];
     }
     else{
         if ($shouldUnserialize) {
             $in = unserialize($in);
         }
-        
     }
+    return $in;
 }
 
 /*$event = $_POST['event'];
@@ -23,12 +23,11 @@ else{
 $event = parseInput($_POST['event'], 'event_name', 'All Events', true);
 $student = parseInput($_POST['student'], 'name', 'All Students', true);
 
-// these may cause problems
 $startDate = parseInput($_POST['startDate'], null, 'None', false);
 $endDate = parseInput($_POST['endDate'], null, 'None', false);
 
-echo "<p>$event</p><br>";
-echo "<p>$student</p><br>";
-echo "<p>Begin Date: $startDate End Date: $endDate</p><br>";
+echo "<p>$event[1]</p>";
+echo "<p>$student[1]</p>";
+echo "<p>Begin Date: $startDate[1] End Date: $endDate[1]</p>";
 
 ?>
